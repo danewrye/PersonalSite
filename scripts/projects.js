e6740d1a6a75160d91defc7ee739dd4016070251
Dane Wrye <danew49@gmail.com>
2020-07-29 02:02:04 -0700

    set up foundation of projects html and js. Added to styles.css

---
+++ scripts/projects.js
@@ -0,0 +1,34 @@
+//Define initial states of tags
+let selected = ["all"];
+let unselected = ["software", "hardware", "design", "front_end", "algorithms", "machine_learning"];
+
+//Find necessary html elements
+const all = document.querySelector(".tags > .all");
+const software = document.querySelector(".tags > .software");
+const hardware = document.querySelector(".tags > .hardware");
+const design = document.querySelector(".tags > .design");
+const front_end = document.querySelector(".tags > .front_end");
+const algorithms = document.querySelector(".tags > .algorithms");
+const machine_learning = document.querySelector(".tags > .machine_learning");
+
+//Setup onclick events for tags
+all.onclick = function() {
+}
+
+software.onclick = function() {
+}
+
+hardware.onclick = function() {
+}
+
+design.onclick = function() {
+}
+
+front_end.onclick = function() {
+}
+
+algorithms.onclick = function() {
+}
+
+machine_learning.onclick = function() {
+}
